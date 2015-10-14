30de2be7bb700c12dae564cb62b2115cb496fce3
Valery Plotnik <6a6kin@gmail.com>
2015-10-14 17:46:51 +0300

    Add UI skeleton

--- ObjRec.UI/MainWindow.Designer.cs
+++ ObjRec.UI/MainWindow.Designer.cs
@@ -28,20 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.sourcePic = new System.Windows.Forms.PictureBox();
+            this.processedPic = new System.Windows.Forms.PictureBox();
+            this.filenameTextbox = new System.Windows.Forms.TextBox();
+            this.loadFileButton = new System.Windows.Forms.Button();
+            this.OtsuFilter = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusBarText = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.processedPic)).BeginInit();
+            this.statusStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // sourcePic
+            // 
+            this.sourcePic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sourcePic.Location = new System.Drawing.Point(12, 39);
+            this.sourcePic.Name = "sourcePic";
+            this.sourcePic.Size = new System.Drawing.Size(640, 480);
+            this.sourcePic.TabIndex = 0;
+            this.sourcePic.TabStop = false;
+            // 
+            // processedPic
+            // 
+            this.processedPic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.processedPic.Location = new System.Drawing.Point(658, 39);
+            this.processedPic.Name = "processedPic";
+            this.processedPic.Size = new System.Drawing.Size(640, 480);
+            this.processedPic.TabIndex = 0;
+            this.processedPic.TabStop = false;
+            // 
+            // filenameTextbox
+            // 
+            this.filenameTextbox.Location = new System.Drawing.Point(12, 12);
+            this.filenameTextbox.Name = "filenameTextbox";
+            this.filenameTextbox.Size = new System.Drawing.Size(640, 20);
+            this.filenameTextbox.TabIndex = 1;
+            this.filenameTextbox.Click += new System.EventHandler(this.filenameTextbox_Click);
+            // 
+            // loadFileButton
+            // 
+            this.loadFileButton.Location = new System.Drawing.Point(656, 10);
+            this.loadFileButton.Name = "loadFileButton";
+            this.loadFileButton.Size = new System.Drawing.Size(75, 23);
+            this.loadFileButton.TabIndex = 2;
+            this.loadFileButton.Text = "Load file";
+            this.loadFileButton.UseVisualStyleBackColor = true;
+            this.loadFileButton.Click += new System.EventHandler(this.loadFileButton_Click);
+            // 
+            // OtsuFilter
+            // 
+            this.OtsuFilter.Location = new System.Drawing.Point(737, 10);
+            this.OtsuFilter.Name = "OtsuFilter";
+            this.OtsuFilter.Size = new System.Drawing.Size(75, 23);
+            this.OtsuFilter.TabIndex = 3;
+            this.OtsuFilter.Text = "Otsu";
+            this.OtsuFilter.UseVisualStyleBackColor = true;
+            this.OtsuFilter.Click += new System.EventHandler(this.OtsuFilter_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBarText});
+            this.statusStrip.Location = new System.Drawing.Point(0, 548);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(1310, 22);
+            this.statusStrip.TabIndex = 4;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // statusBarText
+            // 
+            this.statusBarText.Name = "statusBarText";
+            this.statusBarText.Size = new System.Drawing.Size(39, 17);
+            this.statusBarText.Text = "Ready";
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 534);
+            this.ClientSize = new System.Drawing.Size(1310, 570);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.OtsuFilter);
+            this.Controls.Add(this.loadFileButton);
+            this.Controls.Add(this.filenameTextbox);
+            this.Controls.Add(this.processedPic);
+            this.Controls.Add(this.sourcePic);
             this.Name = "MainWindow";
             this.Text = "Object Recognition";
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.processedPic)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox sourcePic;
+        private System.Windows.Forms.PictureBox processedPic;
+        private System.Windows.Forms.TextBox filenameTextbox;
+        private System.Windows.Forms.Button loadFileButton;
+        private System.Windows.Forms.Button OtsuFilter;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel statusBarText;
     }
 }
 
--- ObjRec.UI/MainWindow.cs
+++ ObjRec.UI/MainWindow.cs
@@ -1,12 +1,47 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using ObjRec.Core.Filters;
 
 namespace ObjRec.UI
 {
     public partial class MainWindow : Form
     {
+        private readonly OpenFileDialog fileDialog = new OpenFileDialog {Filter = @"Picture Files (.bmp)|*.bmp|All Files (*.*)|*.*"};
+
         public MainWindow()
         {
             InitializeComponent();
+
+            fileDialog.FileOk += (sender, args) =>
+            {
+                filenameTextbox.Text = fileDialog.FileName;
+            };
+        }
+
+        private void loadFileButton_Click(object sender, System.EventArgs e)
+        {
+            if (File.Exists(filenameTextbox.Text))
+            {
+                sourcePic.Image = Image.FromFile(filenameTextbox.Text);
+            }
+        }
+
+        private void filenameTextbox_Click(object sender, System.EventArgs e)
+        {
+            fileDialog.Reset();
+            fileDialog.ShowDialog();
+        }
+
+        private async void OtsuFilter_Click(object sender, System.EventArgs e)
+        {
+            //var filter = new ThresholdOtsuFilter();
+
+            statusBarText.Text = "Applying Otsu filter...";
+
+            //processedPic.Image = await filter.Apply(sourcePic.Image);
+
+            statusBarText.Text = "Ready";
         }
     }
 }
