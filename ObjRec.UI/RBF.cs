72e542a30b9271bf88f4f8cef6b6adc85afd85dd
Eugene <eugene.kirvel@gmail.com>
2015-12-16 01:39:35 +0300

    intiall rbf

---
+++ ObjRec.UI/RBF.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.ML;
+using Emgu.CV.Structure;
+
+namespace ObjRec.UI
+{
+    public class RBF
+    {
+        public void Run(int classCount = 5)
+        {
+            using (SVM model = new SVM())
+            {
+                SVMParams p = new SVMParams();
+                p.KernelType = Emgu.CV.ML.MlEnum.SVM_KERNEL_TYPE.RBF;
+                p.SVMType = Emgu.CV.ML.MlEnum.SVM_TYPE.C_SVC;
+                p.C = 1;
+                p.TermCrit = new MCvTermCriteria(100, 0.00001);
+                Matrix<float> trainData = new Matrix<float>(0, 2);
+                Matrix<float> trainClasses = new Matrix<float>(0, 1);
+                //bool trained = model.Train(trainData, trainClasses, null, null, p);
+                bool trained = model.TrainAuto(trainData, trainClasses, null, null, p.MCvSVMParams, classCount);
+
+                for (int i = 0; i < img.Height; i++)
+                {
+                    for (int j = 0; j < img.Width; j++)
+                    {
+                        sample.Data[0, 0] = j;
+                        sample.Data[0, 1] = i;
+
+                        float response = model.Predict(sample);
+
+                        img[i, j] =
+                           response == 1 ? new Bgr(90, 0, 0) :
+                           response == 2 ? new Bgr(0, 90, 0) :
+                           new Bgr(0, 0, 90);
+                    }
+                }
+
+                int c = model.GetSupportVectorCount();
+                for (int i = 0; i < c; i++)
+                {
+                    float[] v = model.GetSupportVector(i);
+                    PointF p1 = new PointF(v[0], v[1]);
+                    img.Draw(new CircleF(p1, 4), new Bgr(128, 128, 128), 2);
+                }
+            }
+        }
+    }
+}
